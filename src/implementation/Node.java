21938b31fd61c6e76ef2bebeea09dfd78b27f287
avanish <avanish.singh_cs.aiml19@gla.ac.in>
2021-04-13 23:36:18 +0530

    inOrder preOrder postOrder reverseInOrder

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -2,4 +2,38 @@ package implementation;
 
 public class Node<E extends Comparable<E>> implements myinterface.Node<E> {
     //complete node class
+    private E data;
+    private myinterface.Node left;
+    private myinterface.Node right;
+
+    public Node(E data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+    public E getData() {
+        return data;
+    }
+
+    @Override
+    public void setData(E data) {
+
+    }
+
+    public myinterface.Node getLeft() {
+        return left;
+    }
+
+
+    public myinterface.Node getRight() {
+        return right;
+    }
+
+    public void setLeft(myinterface.Node left) {
+        this.left = left;
+    }
+
+    public void setRight(myinterface.Node right) {
+        this.right = right;
+    }
 }
--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -1,8 +1,22 @@
 package main;
 
+import implementation.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
         //create binary search tree
         // and perform insert, search, traversal, delete
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(66);
+        tree.insert(77);
+        tree.insert(65);
+        tree.insert(10);
+        tree.insert(53);
+        tree.insert(74);
+        tree.inOrder(tree.getRoot());
+        tree.insert(18);
+        tree.inOrder(tree.getRoot());
+        System.out.println(tree.search(10));
+        System.out.println(tree.search(74));
     }
 }
